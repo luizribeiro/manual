e6fe565f812fb5e47b770646c67acc66689068a5
luiz <luiz@5c36b62e-ec5f-0410-99ac-fb9e63fa3a8a>
2009-01-11 22:11:00 +0000

    Algoritmo de intersecção de segmentos de reta
    git-svn-id: http://svn.luizribeiro.org/maratona/manual@3 5c36b62e-ec5f-0410-99ac-fb9e63fa3a8a

--- algoritmos/template.cpp
+++ algoritmos/template.cpp
@@ -1,5 +1,8 @@
 #define foreach(i, c) for(typeof(c.begin()) i = c.begin(); i != c.end(); i++) 
 
+int min(int a, int b) { return a < b ? a : b; }
+int max(int a, int b) { return a > b ? a : b; }
+
 int main() {
 	/* ... */
 
