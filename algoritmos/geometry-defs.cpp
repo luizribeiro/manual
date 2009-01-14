83b7f5b734ed54cc7bfee07136462e8b653faf27
luiz <luiz@5c36b62e-ec5f-0410-99ac-fb9e63fa3a8a>
2009-01-14 06:01:00 +0000

    Mais algoritmos de geometria computacional
    git-svn-id: http://svn.luizribeiro.org/maratona/manual@4 5c36b62e-ec5f-0410-99ac-fb9e63fa3a8a

---
+++ algoritmos/geometry-defs.cpp
@@ -0,0 +1,2 @@
+typedef struct { int x, y; } point;
+typedef struct { point p[MAX_VERTS]; int n; } polygon;
---
+++ algoritmos/graham-scan.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <vector>
+
+point pivot;
+
+int distsqr(point a, point b) {
+	return (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y);
+}
+
+int cross(point a, point b, point c) {
+	return (b.x - a.x)*(c.y - a.y) - (c.x - a.x)*(b.y - a.y);
+}
+
+bool graham_cmp(point a, point b) {
+	int t = cross(pivot, b, a);
+	if(t < 0) return true;
+	if(t == 0) return distsqr(pivot, a) < distsqr(pivot, b);
+	return false;
+}
+
+/* q = CH(p) */
+void graham_scan(polygon &q, polygon &p) {
+	vector<point> s(p.p, p.p+p.n);
+	int i;
+
+	/* pivot = ponto mais abaixo e mais a esquerda */
+	pivot.x = p.p[0].x;
+	pivot.y = p.p[0].y;
+	for(i = 1; i < p.n; i++) {
+		if(p.p[i].y < pivot.y || (p.p[i].y == pivot.y && p.p[i].x < pivot.x)) {
+			pivot.x = p.p[i].x;
+			pivot.y = p.p[i].y;
+		}
+	}
+
+	/* ordena s no sentido anti-horario, com relacao ao pivot */
+	sort(s.begin(), s.end(), graham_cmp);
+
+	/* remove elementos repetidos de s */
+	for(i = 2; i < (int)s.size(); i++) {
+		if(cross(pivot, s[i], s[i-1]) == 0) {
+			s.erase(s.begin()+i-1);
+			i--;
+		}
+	}
+
+	/* here comes the fun */
+	q.p[0] = s[0];
+	q.p[1] = s[1];
+	q.p[2] = s[2];
+	q.n = 3;
+	for(i = 3; i < (int)s.size(); i++) {
+		while(cross(q.p[q.n-2], q.p[q.n-1], s[i]) <= 0) q.n--;
+		q.p[q.n++] = s[i];
+	}
+}
