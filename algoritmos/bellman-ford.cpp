9d579bbdf4c090a99c781b3d0dcd94f9ba1b5ed7
luiz <luiz@5c36b62e-ec5f-0410-99ac-fb9e63fa3a8a>
2009-05-17 17:46:05 +0000

    Bellman-ford reimplementado
    git-svn-id: http://svn.luizribeiro.org/maratona/manual@13 5c36b62e-ec5f-0410-99ac-fb9e63fa3a8a

--- algoritmos/bellman-ford.cpp
+++ algoritmos/bellman-ford.cpp
@@ -1,28 +1,22 @@
-/* TODO: tornar decente */
-long long bellmanford(int s, int t) {
-	long long di[N];
+int eu[MM], ev[MM]; /* lista de arestas (u,v) (eu[i], ev[i]) */
+int d[NN][NN]; /* matriz de adjacencia */
+
+int bellmanford(int s, int t) {
+	int di[N], p[N];
 	int i, j;
 
-	for(i = 0; i < n; i++) {
-		di[i] = INF;
-		p[i] = -1;
-	}
+	for(i = 0; i < n; i++)
+		di[i] = INF, p[i] = -1;
 
 	di[s] = 0;
 
 	for(j = 0; j < n; j++) {
 		bool trocou = false;
 		for(i = 0; i < m; i++) {
-			int u = eu[i];
-			int v = ev[i];
-			if(cap - f[u][v] > 0 && di[v] > di[u] + d[u][v]) {
+			int u = eu[i], v = ev[i];
+			if(di[v] > di[u] + d[u][v]) {
 				di[v] = di[u] + d[u][v];
 				p[v] = u;
-				trocou = true;
-			}
-			if(cap - f[v][u] > 0 && di[u] > di[v] + d[v][u]) {
-				di[u] = di[v] + d[v][u];
-				p[u] = v;
 				trocou = true;
 			}
 		}
