3ea48f02a08b2714887baee63a894d617391fc84
luiz <luiz@5c36b62e-ec5f-0410-99ac-fb9e63fa3a8a>
2009-08-17 14:57:08 +0000

    Troubleshooting e correcao no poly-area.cpp
    git-svn-id: http://svn.luizribeiro.org/maratona/manual@28 5c36b62e-ec5f-0410-99ac-fb9e63fa3a8a

--- algoritmos/poly-area.cpp
+++ algoritmos/poly-area.cpp
@@ -2,10 +2,10 @@ double polygon_area(polygon q) {
 	double r = 0.0;
 	int i;
 	for(i = 1; i < q.n-1; i++){
-		int x1 = p.p[i].x - p.p[0].x;
-		int y1 = p.p[i].y - p.p[0].y;
-		int x2 = p.p[i+1].x - p.p[0].x;
-		int y2 = p.p[i+1].y - p.p[0].y;
+		int x1 = q.p[i].x - q.p[0].x;
+		int y1 = q.p[i].y - q.p[0].y;
+		int x2 = q.p[i+1].x - q.p[0].x;
+		int y2 = q.p[i+1].y - q.p[0].y;
 		r += x1*y2 - x2*y1;
 	}
 	return fabs(r/2.0);
