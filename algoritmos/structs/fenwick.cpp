e8955f08e1af187bc0927f9d389b438a6104ca4f
Luiz Ribeiro <luizribeiro@gmail.com>
2011-09-15 20:46:21 -0300

    Mudei o codigo da Fenwick Tree

--- algoritmos/structs/fenwick.cpp
+++ algoritmos/structs/fenwick.cpp
@@ -1,15 +1,14 @@
-/* Incrementa val o numero de ocorrencias do valor idx
- * atualizando todos os seus responsaveis <= que maxy
- */
-void update(int idx ,int val){
-	while (idx <= maxy)	tree[idx] += val, idx += (idx & -idx);
-}
-/* Le a frequencia acumulada de todos os numeros
- * ate idx, i.e 1 2 2 2 2 3 4, read(2) retorna 5
- */
-int read(int idx){
+const int FF = 80000;
+int ftree[FF];
+
+int read(int idx) {
 	int sum = 0;
-	if(idx > maxy) idx = maxy;
-	while(idx > 0) sum += tree[idx], idx -= (idx & -idx);
+	for(int i = idx; i; i -= (i & -i))
+		sum += ftree[i];
 	return sum;
+}
+
+void update(int idx, int val) {
+	for(int i = idx; i < FF; i += (i & -i))
+		ftree[i] += val;
 }
