5aed6b2671c305454421413b3fdad1160cf0ab54
Luiz Ribeiro <luizribeiro@gmail.com>
2011-09-16 16:23:55 -0300

    Atualizado KMP

--- algoritmos/others/kmp.cpp
+++ algoritmos/others/kmp.cpp
@@ -1,28 +1,27 @@
-char K[KKL], W[WWL];	// chave de busca, texto
-int F[KKL];				// tabela de falhas
+char text[TT], patt[PP]; // text, pattern
+int n, m; // strlen(text), strlen(pattern)
+int f[PP]; // failure table
 
-void kmp_table(char *K, int KL){
-	F[0] = F[1] = 0;
-	int i, j;
-	for(i = 2; i < KL; ++i){
-		j = F[i - 1];
-		while(1){
-			if(K[j] == K[i - 1]){ F[i] = j + 1; break; }
-			if(j == 0){ F[i] = 0; break; }
-			j = F[j];
-		}
+void buildf(void) {
+	f[0] = f[1] = 0;
+	for(int i = 2; i <= m; i++) {
+		f[i] = -1;
+		for(int j = f[i-1]; f[i] == -1; j = f[j])
+			if(patt[j] == patt[i-1]) f[i] = j+1;
+			else if(j == 0) f[i] = 0;
 	}
 }
 
-int kmp(char *W, int WL, char *K, int KL){
-	kmp_table(K, KL);
-	for(int i = 0, j = 0; j < WL; ){
-		if(W[j] == K[i]){
-			i++;
-			j++;
-			if(i == KL) return j;
-		} else if(i > 0) i = F[i];
-		else j++;
+void kmp(void) {
+	buildf();
+	for(int i = 0, j = 0; j < n; j++) {
+		// i = automaton's state, j = text position
+		if(text[j] == patt[i]) {
+			i++; // move to next state
+			if(i == m) {
+				printf("found match at index %d\n", j-m+1);
+				i = f[i];
+			}
+		} else if(i) i = f[i], j--;
 	}
-	return -1;
 }
