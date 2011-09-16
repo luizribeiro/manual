0ea3c0a11dce431b3e8f01c48fbd5f9a0fcdc6fd
Luiz Ribeiro <luizribeiro@gmail.com>
2011-09-16 16:14:58 -0300

    Atualizado Edmonds-Karp

--- algoritmos/graph/ford-fulkerson.cpp
+++ algoritmos/graph/ford-fulkerson.cpp
@@ -1,50 +1,50 @@
-int n;						/* numero de vertices */
-int c[NN][NN], f[NN][NN];	/* capacidade, fluxo */
-int v[NN], p[NN];			/* visitado, predecessor */
+const int INF = 0x3f3f3f3f;
+
+int nno, ned;
+struct edge {
+	int u, v, cap, flow, next;
+	edge(int _u, int _v, int _cap, int _next = -1) :
+		u(_u), v(_v), cap(_cap), next(_next) { flow = 0; }
+};
+vector<edge> edges;
+vector<int> first, prev;
 
 bool bfs(int s, int t) {
+	vector<int> vis(nno, 0);
 	queue<int> q;
-	int i, j;
-	for(i = 0; i < n; i++)
-		v[i] = 0;
-	q.push(s);
-	v[s] = 1;
-	p[s] = -1;
-	while(!q.empty() && v[t] == 0) {
-		i = q.front();
-		q.pop();
-		for(j = 0; j < n; j++) {
-			if(v[j] == 0 && c[i][j] - f[i][j] > 0) {
-				q.push(j);
-				v[j] = 1;
-				p[j] = i;
+	prev = vector<int>(nno, -1);
+	q.push(s), vis[s] = 1;
+	while(!q.empty() && !vis[t]) {
+		s = q.front(); q.pop();
+		for(int i = first[s]; i != -1; i = edges[i].next) {
+			if(!vis[edges[i].v] && edges[i].cap - edges[i].flow > 0) {
+				q.push(edges[i].v), vis[edges[i].v] = 1;
+				prev[edges[i].v] = i;
 			}
 		}
 	}
-	return v[t] == 1;
+	return vis[t];
 }
 
-int max_flow(int s, int t) {
+int maxflow(int s, int t) {
 	int mf = 0;
-	int i, j;
-
-	for(i = 0; i < n; i++)
-		for(j = 0; j < n; j++)
-			f[i][j] = 0;
-
 	while(bfs(s, t)) {
 		int cf = INF;
-
-		for(i = t; p[i] >= 0; i = p[i])
-			cf = min(cf, c[p[i]][i] - f[p[i]][i]);
-
-		for(i = t; p[i] >= 0; i = p[i]) {
-			f[p[i]][i] += cf;
-			f[i][p[i]] -= cf;
-		}
-
+		for(int i = prev[t]; i != -1; i = prev[edges[i].u])
+			cf = min(cf, edges[i].cap - edges[i].flow);
+		for(int i = prev[t]; i != -1; i = prev[edges[i].u])
+			edges[i].flow += cf, edges[i^1].flow -= cf;
 		mf += cf;
 	}
-
 	return mf;
+}
+
+void reset(int n) {
+	nno = n, ned = 0, edges.clear();
+	first = vector<int>(nno, -1);
+}
+
+void addedge(int u, int v, int cap) {
+	edges.push_back(edge(u, v, cap, first[u])); first[u] = ned++;
+	edges.push_back(edge(v, u,   0, first[v])); first[v] = ned++;
 }
