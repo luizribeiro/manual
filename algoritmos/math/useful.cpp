04a8a52efe42a2dc9d78bfd77c15b3149c8cb7ea
Luiz Ribeiro <luizribeiro@gmail.com>
2011-09-16 16:09:56 -0300

    Adicionados primos úteis pra hashing

---
+++ algoritmos/math/useful.cpp
@@ -0,0 +1,13 @@
+Largest Prime Numbers
+---------------------
+For signed 32 bit values:
+    2^31-1 = 0x7FFFFFFF = 2,147,483,647
+
+For unsigned 32 bit values:
+    2^32-5 = 0xFFFFFFFB = 4,294,967,291
+
+Random Big Prime Numbers
+------------------------
+16,411      21,391      47,533      53,887      552,317
+553,093     687,131     6,674,747   15,485,867  21,263,399
+245,680,349 442,092,389
